959149e902b880705afbd878704070edbd8b3071
Hardik Barvaliya <32429687+Hardik501@users.noreply.github.com>
2020-06-24 15:43:51 +0530

    updated!!!!

--- app.js
+++ app.js
@@ -11,14 +11,34 @@ GAME RULES:
 var score = [0,0];
 var turn = 0;
 var score_holder = 0;
+var pre_dice;
+var pre_dice1;
+var win =100;
+var game = true;
 
 document.querySelector('.btn-roll').addEventListener('click',function(){
-    dice = Math.floor(Math.random()*6)+1;
-    score_holder +=dice;
+    var dice = Math.floor(Math.random()*6)+1;
+    var dice1 = Math.floor(Math.random()*6)+1;
+    if(game){
+    score_holder +=dice+dice1;
     let dice_src = 'dice-'+dice+'.png';
+    let dice1_src = 'dice-'+dice1+'.png';
     document.getElementById('score-'+turn).innerHTML = score_holder;
     document.querySelector('.dice').src =  dice_src;
-    if(dice==1){
+    document.querySelector('.dice-1').src =  dice1_src;
+    if(dice === 6 && pre_dice===6 || dice1 === 6 && pre_dice1 === 6 ){
+        turn = turn==0?1:0;
+        if(turn===1){
+            score_holder = 0;
+            document.querySelector('.player-0-panel').classList.remove('active');
+            document.querySelector('.player-1-panel').classList.add('active');
+        }
+        else{
+            document.querySelector('.player-1-panel').classList.remove('active');
+            document.querySelector('.player-0-panel').classList.add('active');
+        } 
+    }
+    else if(dice==1 ||dice1==1){
         score_holder =0;
         turn = turn==0?1:0;
         if(turn===1){
@@ -30,15 +50,24 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
             document.querySelector('.player-0-panel').classList.add('active');
         }
     }
+    pre_dice = dice;
+    pre_dice1 = dice;
+    }
+});
+
+document.querySelector('.btn-win').addEventListener('click',function(){
+    win = prompt("Enter the winning score:",100);
 });
 document.querySelector('.btn-hold').addEventListener('click',function(){
+    if(game){
     score[turn] += score_holder;
     document.getElementById('current-'+turn).innerHTML = score[turn];
     document.getElementById('score-'+turn).innerHTML = 0;
-    score_holder = 0;
-    if(score[turn]>=100){
-        document.querySelector('.winner').textContent = "  Winner is player "+Number(turn+1);
+    if(score[turn]>=Number(win)){
+        document.getElementById('name-'+turn).textContent = "Winner!!!";
+        game = false;
     }
+    score_holder = 0;
     turn = turn==0?1:0;
     if(turn===1){
         document.querySelector('.player-0-panel').classList.remove('active');
@@ -47,7 +76,8 @@ document.querySelector('.btn-hold').addEventListener('click',function(){
     else{
         document.querySelector('.player-1-panel').classList.remove('active');
         document.querySelector('.player-0-panel').classList.add('active');
-    }    
+    }
+}
 });
 
 
@@ -56,7 +86,12 @@ document.querySelector('.btn-new').addEventListener('click',function(){
     document.getElementById('current-1').innerHTML = 0;
     document.getElementById('score-0').innerHTML = 0;
     document.getElementById('score-1').innerHTML = 0;
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
     score_holder = 0;
     turn = 0;
-    document.querySelector('.winner').textContent = "";
+    score = [0,0];
+    game = true;
+    document.getElementById('name-0').textContent = "Player 1";
+    document.getElementById('name-1').textContent = "Player 2";
 });
